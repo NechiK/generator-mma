8a7257029468558d1a1e092d6234d0359faf5424
maksnechesonov <maks.nechesonov@agilie.com>
2017-03-06 12:02:06 +0200

    - added makefile for generate task - adjusted package.json - adjusted config js

--- app/templates/src/client/app/core/config.js
+++ app/templates/src/client/app/core/config.js
@@ -4,45 +4,53 @@
     var core = angular.module('app.core');
 
     core.config(configFunction);
+    core.config(requestsConfig);
 
-    configFunction.$inject = ['toastr', '$httpProvider', '$resourceProvider'];
+    configFunction.$inject = ['toastr'];
     /* @ngInject */
-    function configFunction(toastr, $httpProvider, $resourceProvider) {
+    function configFunction(toastr) {
         toastr.options.timeOut = 4000;
         toastr.options.positionClass = 'toast-bottom-right';
-
-        // $resourceProvider.defaults.actions.post = {
-        //     method: 'POST'
-        // };
-        //
-        // $httpProvider.defaults.withCredentials = true;
-        // $httpProvider.interceptors.push(interceptor);
     }
 
-    // interceptor.$inject = ['config'];
-    // function interceptor(config) {
-    //     return {
-    //         request: function(requestConfig) {
-    //             if (!isApiRequest(requestConfig.url)) {
-    //                 return requestConfig;
-    //             }
-    //
-    //         },
-    //         response: function (response) {
-    //             if (isApiRequest(response.config.url)) {
-    //
-    //             }
-    //             return response;
-    //         },
-    //         responseError: function (error, ajaxOptions, thrownError) {
-    //
-    //         }
-    //     };
-    //
-    //     function isApiRequest(url) {
-    //         return url.indexOf(config.domainProtocol + config.domainEndpoint + config.portEndpoint) === 0;
-    //     }
-    // }
+    <% if (requestsArchitect) {%>
+    requestsConfig.$inject = ['$httpProvider', '$resourceProvider'];
+    /* @ngInject */
+    function requestsConfig($httpProvider, $resourceProvider) {
+        $resourceProvider.defaults.actions.post = {
+            method: 'POST'
+        };
+
+        $httpProvider.defaults.withCredentials = true;
+        $httpProvider.interceptors.push(interceptor);
+    }
+
+    interceptor.$inject = ['config'];
+    function interceptor(config) {
+        return {
+            request: function(requestConfig) {
+                if (!isApiRequest(requestConfig.url)) {
+                    return requestConfig;
+                }
+
+            },
+            response: function (response) {
+                if (isApiRequest(response.config.url)) {
+
+                }
+                return response;
+            },
+            responseError: function (error, ajaxOptions, thrownError) {
+
+            }
+        };
+
+        function isApiRequest(url) {
+            return url.indexOf(config.domainProtocol + config.domainEndpoint + config.portEndpoint) === 0;
+        }
+    }
+
+    <% } %>
 
     var config = {
         appErrorPrefix: '[<%= appName %> Error] ',
